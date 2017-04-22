df34f745a5a58f3fb4d36f692eec848f6a618e66
x4v13r0 <x4v13r0.o@free.fr>
2017-04-22 16:46:23 +0000

    Separation of the web figure creation in _web. HW intialisation moved to _fun module

--- WStation_web.py
+++ WStation_web.py
@@ -1,7 +1,7 @@
 import time
 import WStation_fun as WS
 
-# Web Figures
+# Web Figures & Configuration
 dayinsec=24*60*60
 weekinsec=dayinsec*7
 monthinsec=dayinsec*30
@@ -11,16 +11,20 @@ durs=[dayinsec,weekinsec,monthinsec,yearinsec]
 durnames=['day','week','month','year']
 web_figs  =['web_figure_1','web_figure_2','web_figure_3']
 web_titles=['Temperature (dgC)','Pressure (hPa)','Relative humidity (%)']
-# Web Figures initialization
-WS.gen_web_curve(logfile,web_figs,web_titles,durs,durnames)
+# --> ex: /var/www/html/web_figure_1_day.png
 ##############################################################################
 refresh_dur=60.       # refresh duration in s
 logfile='WS_log.txt'  # logfile to plot
 
+# Web Figures initialization
+print "Initializing web curves"
+WS.gen_web_curve(logfile,web_figs,web_titles,durs,durnames)
+
 prev_time=time.time()
 
 while True:
+    cur_time=time.time()
     if time.time()-prev_time>refresh_dur:
-        print "Updating web curves"
+        print 'Updating web curves @%.2f' % cur_time
         WS.gen_web_curve(logfile,web_figs,web_titles,durs,durnames)
         prev_time=time.time()
